27a54250ccd3e4f8f8962c9ec6b6df3fc88ec6a2
Erick Amoedo <erick.amoedo@live.com>
2020-08-30 12:52:44 -0300

    v3
    voltando os files

---
+++ js/scripts.js
@@ -0,0 +1,192 @@
+/* Template: Como - Coming Soon Landing Page Template
+   Author: Inovatik
+   Description: Custom JS file
+*/
+
+
+(function($) {
+    "use strict"; 
+    
+    /* Particles */
+	particlesJS("particles-js", {
+		"particles": {
+		"number": {
+			"value": 120,
+			"density": {
+			"enable": true,
+			"value_area": 1200
+			}
+		},
+		"color": {
+			"value": "#585f63"
+		},
+		"shape": {
+			"type": "circle",
+			"stroke": {
+			"width": 0,
+			"color": "#000000"
+			},
+			"polygon": {
+			"nb_sides": 5
+			},
+			"image": {
+			"src": "img/github.svg",
+			"width": 100,
+			"height": 100
+			}
+		},
+		"opacity": {
+			"value": 0.2,
+			"random": false,
+			"anim": {
+			"enable": false,
+			"speed": 1,
+			"opacity_min": 0.2,
+			"sync": false
+			}
+		},
+		"size": {
+			"value": 3,
+			"random": true,
+			"anim": {
+			"enable": false,
+			"speed": 40,
+			"size_min": 0.1,
+			"sync": false
+			}
+		},
+		"line_linked": {
+			"enable": true,
+			"distance": 150,
+			"color": "#585f63",
+			"opacity": 0.4,
+			"width": 1
+		},
+		"move": {
+			"enable": true,
+			"speed": 6,
+			"direction": "none",
+			"random": false,
+			"straight": false,
+			"out_mode": "out",
+			"bounce": false,
+			"attract": {
+			"enable": false,
+			"rotateX": 600,
+			"rotateY": 1200
+			}
+		}
+		},
+		"interactivity": {
+		"detect_on": "canvas",
+		"events": {
+			"onhover": {
+			"enable": true,
+			"mode": "grab"
+			},
+			"onclick": {
+			"enable": true,
+			"mode": "push"
+			},
+			"resize": true
+		},
+		"modes": {
+			"grab": {
+			"distance": 140,
+			"line_linked": {
+				"opacity": 1
+			}
+			},
+			"bubble": {
+			"distance": 400,
+			"size": 40,
+			"duration": 2,
+			"opacity": 8,
+			"speed": 3
+			},
+			"repulse": {
+			"distance": 200,
+			"duration": 0.4
+			},
+			"push": {
+			"particles_nb": 4
+			},
+			"remove": {
+			"particles_nb": 2
+			}
+		}
+		},
+		"retina_detect": true
+    });
+    
+
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
+    $("input, textarea").keyup(function(){
+		if ($(this).val() != '') {
+			$(this).addClass('notEmpty');
+		} else {
+			$(this).removeClass('notEmpty');
+		}
+    });
+
+
+    /* Sign Up Form */
+    $("#signUpForm").validator().on("submit", function(event) {
+    	if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            sformError();
+            ssubmitMSG(false, "Please fill all fields!");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            ssubmitForm();
+        }
+    });
+
+    function ssubmitForm() {
+        // initiate variables with form content
+		var email = $("#semail").val();
+        $.ajax({
+            type: "POST",
+            url: "php/signupform-process.php",
+            data: "email=" + email, 
+            success: function(text) {
+                if (text == "success") {
+                    sformSuccess();
+                } else {
+                    sformError();
+                    ssubmitMSG(false, text);
+                }
+            }
+        });
+	}
+
+    function sformSuccess() {
+        $("#signUpForm")[0].reset();
+        ssubmitMSG(true, "Signed Up!");
+        $("input").removeClass('notEmpty'); // resets the field label after submission
+    }
+
+    function sformError() {
+        $("#signUpForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass();
+        });
+	}
+
+    function ssubmitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center tada animated";
+        } else {
+            var msgClasses = "h3 text-center";
+        }
+        $("#smsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function() {
+		$(this).blur();
+	});
+
+})(jQuery);
